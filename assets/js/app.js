c723108ddd23779be8bfcdd4a06a9a4f57cda44b
usaidiqbal <usaid.techxcape@gmail.com>
2021-10-16 11:42:36 +0500

    demo completed

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+const services = document.querySelector('#services');
+const servicesLinks = document.querySelector('#services-links-menu');
+services.addEventListener('click', function(){
+    servicesLinks.classList.add('services-links-h');
+});
