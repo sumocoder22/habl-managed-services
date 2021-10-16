a3939637f0ed1d55ed1e50ca13eb34de4c69f533
unknown <usaidp10@gmail.com>
2021-10-16 22:05:30 +0500

    loader added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+setInterval(function(){ 
+    document.getElementById('preloader').style.opacity = 0;
+    document.getElementById('preloader').style.zIndex = 0
+}, 3000);
+
 const services = document.querySelector('#services');
 const servicesLinks = document.querySelector('#services-links-menu');
 services.addEventListener('click', function(){
