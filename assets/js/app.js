c5ee58185baed466dfe07ec6321a2909c13495e3
unknown <usaid.techxcape@gmail.com>
2021-10-23 16:51:33 +0500

    services links hover issue resolved

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
-setInterval(function(){ 
+setTimeout(function(){ 
+    console.log('i am running');
     document.getElementById('preloader').style.opacity = 0;
-    document.getElementById('preloader').style.zIndex = 0
+    // document.getElementById('preloader').style.zIndex = 0;
+    setTimeout(function(){
+        document.getElementById('preloader').style.display = 'none';
+     }, 1000)
 }, 3000);
 
 const services = document.querySelector('#services');
 const servicesLinks = document.querySelector('#services-links-menu');
+
 services.addEventListener('click', function(){
-    servicesLinks.classList.add('services-links-h');
+    servicesLinks.classList.toggle('services-links-h');
 });
